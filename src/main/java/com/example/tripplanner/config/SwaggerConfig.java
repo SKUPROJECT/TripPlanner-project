ec884d64079813a64a60d266a8f41536a9a1367a
sun8183 <4sunskyhyun@gmail.com>
2024-09-05 22:56:10 +0900

    Swagger 설정 및 API 준비

---
+++ src/main/java/com/example/tripplanner/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.tripplanner.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Trip Planner API")
+                .description("여행지 추천 프로젝트 API")
+                .version("1.0.0");
+    }
+}
