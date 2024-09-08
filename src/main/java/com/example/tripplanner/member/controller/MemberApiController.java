7f98ac3a5790b54c7feb933826fee4b7655f17ef
angel107 <hll1107@naver.com>
2024-09-08 10:53:43 +0900

    20240908 c

--- src/main/java/com/example/tripplanner/member/controller/MemberApiController.java
+++ src/main/java/com/example/tripplanner/member/controller/MemberApiController.java
@@ -1,6 +1,7 @@
 package com.example.tripplanner.member.controller;
 
 import com.example.tripplanner.member.dto.AddMemberRequest;
+import com.example.tripplanner.member.dto.AddMemberResponse;
 import com.example.tripplanner.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,9 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,10 +21,20 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/user")
-    public String signup(AddMemberRequest request){
+    @ResponseBody
+    public AddMemberResponse signup(AddMemberRequest request){
         memberService.save(request);
-        return "redirect:/login";
+        AddMemberResponse response = new AddMemberResponse();
+
+        return response ;
     }
+
+//    public HashMap<String, String> signup(AddMemberRequest request){
+//        memberService.save(request);
+//        HashMap<String,String> map = new HashMap<>();
+//        map.put("a","a");
+//        return map;
+//    } 데이터로 처리하는 방식
 
     @GetMapping("/api/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response){
